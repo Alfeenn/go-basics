2019c1ae6c0d71d12f6fb61464402338c930a512
Alfian <alfiandwi.yanto@gmail.com>
2022-06-25 21:45:05 +0700

    update v1.2.0

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Alfeenn/go-basics
 
 go 1.18
+
+require github.com/Alfeenn/go v0.0.0-20220624164609-ecf699f4d14b // indirect
--- main.go
+++ main.go
@@ -2,23 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 type People struct {
 	a1 string `alias : "num1"`
 	a2 string `alias : ""`
-	a3 string 
+	a3 string
 }
 
 func main() {
 
-	p1 := People{}
-	sample := reflect.TypeOf(p1)
-
-	for i:= 0; i<sample.NumField();i++{
-
-
-		}
+	fmt.Println("hello world")
 
 }
